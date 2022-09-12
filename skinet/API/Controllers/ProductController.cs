73c5f30db5c7941559d1ccfd469cc19450b47fb6
johnnytruong2702 <71922983+johnnytruong2702@users.noreply.github.com>
2022-09-12 11:31:37 +1000

    Add files via upload

---
+++ skinet/API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController ]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = _context.Products.ToList();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "single product";
+        }
+    }
+}
